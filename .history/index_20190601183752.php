f0d68d6e6aa61914eb11fb381a22f034b96e762b
Artur Robek <robek.artur@gmial.com>
2019-08-09 12:04:03 +0200

    init commit

---
+++ .history/index_20190601183752.php
@@ -0,0 +1,86 @@
+<?php include('includes/header.php')?>
+
+
+        <!-- sec2 -->
+        <section id="sec2">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-6">
+                        <h1>OFERTA</h1>
+                    </div>
+                </div>
+                <div class="row justify-content-between">
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 ml-auto">
+                    <h2>USŁUGI</h2>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 mr-auto">
+                    <h2>USŁUGI PODATKOWE</h2>
+                    </div>
+                </div>
+                <div class="row justify-content-between">
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 ml-auto">
+                        <ul>
+                            <li>pomoc w założeniu firmy</li>
+                            <li>pomoc w uzyskaniu dotacji z PUP</li>
+                            <li>pozyskiwanie refundacji z PFRAN</li>
+                        </ul>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 mr-auto">
+                        <ul>
+                            <li>sporządzanie pism podań</li>
+                            <li>sporządzanie pism urzędowych</li>
+                            <li>sporządzanie biznes planów</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="row justify-content-between">
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 ml-auto">
+                    <h2>KOMPLEKSOWA USŁUGA FINANSOWO-KSIĘGOWA</h2>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 mr-auto">
+                    <h2><br>DLA WIERZYCIELI</h2>
+                    </div>
+                </div>
+                <div class="row justify-content-between">
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 ml-auto">
+                        <ul>
+                            <li>Krzążki Przychodów i Rozchodów</li>
+                            <li>Ryczałt</li>
+                            <li>Kadry i Płace</li>
+                            <li>Rozliczenia z ZUS</li>
+                            <li>BHP</li>
+                            <li>rozliczenia roczne PIT</li>
+                        </ul>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5 col-5 mr-auto">
+                        <ul>
+                            <li>wnioski o zwrot niewypłaconych wynagrodzeń FGŚP</li>
+                            <li>elektroniczne pozwy</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--end sec2-->
+        
+        <!--sec3-->
+        <section id="sec3">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
+                        <h1>O MNIE</h1>         
+                    </div>
+                    <div class="row justify-content-center">
+                        <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10 col-12">
+                            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10 col-12">
+                            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- end sec3-->
+        
+        <?php include('includes/footer.php'); ?>
---
+++ includes/footer.php
@@ -0,0 +1,32 @@
+<!-- sec4-->
+        <section id="sec4">
+            <div class="container">
+                <div class="row justify-content-center" id="kontakt">
+                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">                            <h1>KONTAKT</h1>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-6">
+                        <p class="text-center">ul. Zebrzydowicka 32/2, 44-210 Rybnik</p>
+                        <p class="text-center">+48 511 474 064</p> <p class="text-center">biuro@beatalukas.pl</p>
+                    </div>
+                </div>
+                <div class="row justify-content-end">
+                    <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4 col-5">
+                        <footer>
+                            <p>&copy;2019 Artur Robek</p>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- end sec4-->
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
