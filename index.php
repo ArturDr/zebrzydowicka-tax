982670668048db36c760852bac359a57d95acee6
Artur Robek <robek.artur@gmial.com>
2020-05-23 11:13:25 +0200

    change </div>

--- index.php
+++ index.php
@@ -70,13 +70,13 @@
                     <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6">
                         <h1>O MNIE</h1>
                     </div>
-                    <div class="row justify-content-center">
-                        <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10 col-12">
-                            <p class="text-center">Zawodowo jestem związana z finansami od 1997 roku. W pierwszych latach swojej kariery byłam pracownikiem podmiotów państwowych (Urząd Skarbowy, ZUS), gdzie zajmowałam się m. in. podatkiem dochodowym od osób fizycznych, monitorowaniem zadłużenia oraz egzekucją składek. Od 2002 roku rozpoczęłam pracę jako specjalistka ds. kadr i płac oraz księgowa. Moje bogate doświadczenie zawodowe pozawala mi, szeroko spojrzeć na sprawy finansowe klienta.</p>
-                        </div>
-                        <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10 col-12">
-                            <p class="text-center">Firmę zebrzydowickaTAX założyłam w połowie 2016 roku. Stawiam na najwyższą jakość wykonywanych usług oraz terminowość działań. Oferuję szeroki zakres usług kadrowych i płacowych oraz pomoc przy założeniu i prowadzeniu działalności gospodarczej.</p>
-                        </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10 col-12">
+                        <p class="text-center">Zawodowo jestem związana z finansami od 1997 roku. W pierwszych latach swojej kariery byłam pracownikiem podmiotów państwowych (Urząd Skarbowy, ZUS), gdzie zajmowałam się m. in. podatkiem dochodowym od osób fizycznych, monitorowaniem zadłużenia oraz egzekucją składek. Od 2002 roku rozpoczęłam pracę jako specjalistka ds. kadr i płac oraz księgowa. Moje bogate doświadczenie zawodowe pozawala mi, szeroko spojrzeć na sprawy finansowe klienta.</p>
+                    </div>
+                    <div class="col-xl-8 col-lg-8 col-md-10 col-sm-10 col-12">
+                        <p class="text-center">Firmę zebrzydowickaTAX założyłam w połowie 2016 roku. Stawiam na najwyższą jakość wykonywanych usług oraz terminowość działań. Oferuję szeroki zakres usług kadrowych i płacowych oraz pomoc przy założeniu i prowadzeniu działalności gospodarczej.</p>
                     </div>
                 </div>
             </div>
